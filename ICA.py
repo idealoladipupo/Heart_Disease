68bd852621c5f3f9c7f520701599a5a358019eb7
idealoladipupo <oladipupo53@gmail.com>
2024-02-07 12:12:37 +0000

    import data visualisation libraries

--- ICA.py
+++ ICA.py
@@ -1,3 +1,7 @@
 # IMPORT DATA MANIPULATION AND ANALYSIS LIBRARIES
 import pandas as pd
 import numpy as np
+
+# IMPORT DATA VISUALIZATION LIBRARIES
+import matplotlib.pyplot as plt
+import seaborn as sns
