9fd72c2b4190202f49daa4309911c9741569b7dd
idealoladipupo <oladipupo53@gmail.com>
2024-02-07 17:42:59 +0000

    check missing values, visualizations and ploting correlation matrix

--- ICA.py
+++ ICA.py
@@ -21,3 +21,30 @@ from sklearn.model_selection import cross_val_score, GridSearchCV
 
 # import dataset
 mydf = pd.read_csv("heart.csv")
+print(mydf)
+
+print(mydf.describe())
+
+print("\nFirst Few Rows in the Datset:")
+print(mydf.head())
+
+print("\nLast Few Rows in the Datset:")
+print(mydf.tail())
+
+
+print("\nChecking for Missing Values in the Datset:")
+print(mydf.isnull().sum())
+
+
+# Visualise the distributioof numerical features
+# age distribution of patients
+plt.figure(figsize=(12, 8))
+sns.histplot(data=mydf, x="age", kde=True)
+plt.title(" Age Distribution of Patients")
+plt.show()
+
+# Corellation Matrix Plot
+plt.figure(figsize=(10, 8))
+sns.heatmap(mydf.corr(), annot=True, cmap="coolwarm", fmt=".2f", linewidths=0.5)
+plt.title("Correlation Matrix of Numerical Features")
+plt.show()
