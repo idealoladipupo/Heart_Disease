c7e1c371ba63ffcf6124c0d5062eaa36a73a9884
idealoladipupo <oladipupo53@gmail.com>
2024-02-07 12:14:44 +0000

    import machine learning libraries

--- ICA.py
+++ ICA.py
@@ -5,3 +5,13 @@ import numpy as np
 # IMPORT DATA VISUALIZATION LIBRARIES
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# IMPORT MACHINE LEARNING LIBRARIES
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+from sklearn import metrics
